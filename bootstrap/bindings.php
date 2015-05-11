082fbb662c0f71158eccebe3d91b832c669ea1cb
brunty <openblue555@gmail.com>
2015-05-11 15:00:03 +0100

    Bindings update

--- bootstrap/bindings.php
+++ bootstrap/bindings.php
@@ -1,12 +1,14 @@
 <?php
 use Brunty\App;
+use Klein\Klein;
+use Predis\Client as Redis;
 
 $app = new App;
 
 $app['router'] = function ($c) {
-    return new \Klein\Klein();
+    return new Klein();
 };
 
 $app['redis'] = function($c) {
-    return new Predis\Client();
+    return new Redis();
 };
